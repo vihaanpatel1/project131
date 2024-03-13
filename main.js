f42843d6cd7176308911ab1db22237812525ed69
vihaanpatel1 <135666708+vihaanpatel1@users.noreply.github.com>
2024-03-13 16:44:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    Reflect(30, 60, 450, 350);
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center()
+}
